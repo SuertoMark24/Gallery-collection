70bd948996a710b327dfe97bc616a6ae367ed33a
marksuerto <mark@2doit.nu>
2022-02-22 14:00:25 +0800

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,8 @@ namespace Galkery.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
